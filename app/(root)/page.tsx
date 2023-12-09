4d43bcfb9ea22e25e92347d775443176f97c7004
shehab124 <chehab.gamal@gmail.com>
2023-12-09 03:39:07 +0200

    Navbar and leftsidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,7 @@
-import { UserButton } from "@clerk/nextjs"
-
 export default function Home() {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
+    <>
+      <h1 className="text-black head-text text-left">Home</h1>
+    </>
   )
 }
