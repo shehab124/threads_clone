a544203df89b576a87dd0f55751dc28a07c32550
shehab124 <chehab.gamal@gmail.com>
2023-12-09 22:30:24 +0200

    Leftsidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,22 +1,55 @@
+"use client"
+
 import Link from 'next/link'
 import Image from 'next/image'
+import { useRouter, usePathname } from 'next/navigation'
 import { sidebarLinks } from '@/constants/index'
+import { SignedIn, SignOutButton } from '@clerk/nextjs'
 
 export default function LeftSidebar() {
+
+    const router = useRouter();
+    const path = usePathname();
+
     return (
         <section className="custom-scrollbar leftsidebar">
             <div className="flex w-full flex-1 flex-col gap-6 px-6">
                 {
-                    sidebarLinks.map((link) => (
-                        <Link href={link.route} key={link.label} className='leftsidebar_link'>
+                    sidebarLinks.map((link) => {
 
-                            <Image src={link.imgURL} alt={link.label} width={24} height={24} />
-                            <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+                        const isActive = (path.includes(link.route) && link.route.length > 1)
+                            || path === link.route;
 
-                        </Link>
-                    ))
+                        return (
+                            <Link
+                                href={link.route}
+                                key={link.label}
+                                className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
+                            >
+
+                                <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+                                <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+
+                            </Link>
+                        )
+                    })
                 }
             </div>
+
+            {/*Logout*/}
+            <div className='mt-10 px-6'>
+                <SignedIn>
+                    <SignOutButton signOutCallback={() => {
+                        router.push('/sign-in')
+                    }}>
+                        <div className='flex cursor-pointer gap-4 p-4'>
+                            <Image src="/assets/logout.svg" alt='logout' width={24} height={24} />
+                            <p className='text-light-2 max-lg:hidden'>Logout</p>
+                        </div>
+                    </SignOutButton>
+                </SignedIn>
+            </div>
+
         </section>
     )
 }
