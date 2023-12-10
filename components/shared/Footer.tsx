3aa25bd4c3de06eada688a58a5593f8e426ef84f
shehab124 <chehab.gamal@gmail.com>
2023-12-11 01:09:33 +0200

    installed shadcn

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,41 @@
+"use client"
+
+import { useRouter, usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link"
+import Image from "next/image"
+
 export default function Footer() {
+
+    const router = useRouter();
+    const path = usePathname();
+
     return (
-        <div>Footer</div>
+        <section className="bottombar">
+            <div className="bottombar_container">
+                {
+                    sidebarLinks.map((link) => {
+
+                        const isActive = (path.includes(link.route) && link.route.length > 1)
+                            || path === link.route;
+
+                        return (
+                            <Link
+                                href={link.route}
+                                key={link.label}
+                                className={`bottombar_link ${isActive && 'bg-primary-500'}`}
+                            >
+
+                                <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+                                <p className='text-subtle-medium text-light-1 max-sm:hidden'>
+                                    {link.label.split(/\s+/)[0]}
+                                </p>
+
+                            </Link>
+                        )
+                    })
+                }
+            </div>
+        </section>
     )
 }
--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { SignedIn, SignOutButton, OrganizationSwitcher } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 
 export default function Navbar() {
     return (
         <nav className="topbar">
+            {/*Logo*/}
             <Link href='/' className='flex items-center gap-4'>
                 <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
                 <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
@@ -25,6 +27,7 @@ export default function Navbar() {
                 {/*Organiztion switcher*/}
                 <OrganizationSwitcher
                     appearance={{
+                        baseTheme: dark,
                         elements: {
                             organizationSwitcherTrigger: "py-2 px-4"
                         }
